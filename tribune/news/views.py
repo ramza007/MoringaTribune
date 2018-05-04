0529e649f75cb06cda591e64d06816075678e231
ramza007 <ramza007@gmail.com>
2018-05-04 11:33:03 +0300

    added stying

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, Http404
 import datetime as dt 
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from .models import Article
 
 # views
